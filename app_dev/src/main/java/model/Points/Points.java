c88ec63912eea7e91b90648e792f02c2d50a123b
NTum07 <nicola.tummolo@mail.polimi.it>
2024-03-14 23:47:15 +0100

    GameState e StrategyPattern

---
+++ app_dev/src/main/java/model/Points/Points.java
@@ -0,0 +1,9 @@
+package model.Points;
+
+abstract class Points {
+
+    public int countPoints(){
+
+        return 0;
+    }
+}
---
+++ app_dev/src/main/java/model/Points/ResourcesPoints.java
@@ -0,0 +1,14 @@
+package model.Points;
+
+import model.enums.Artifact;
+import model.enums.Element;
+
+public class ResourcesPoints extends Points{
+
+    Element element = Element.ANIMALS;
+    Artifact artifact = Artifact.INK;
+    public int Points(){
+
+        return 0;
+    }
+}
