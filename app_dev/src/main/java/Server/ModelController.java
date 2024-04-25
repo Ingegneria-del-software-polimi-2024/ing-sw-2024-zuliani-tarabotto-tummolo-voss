a167d1eaef75827ee785a6beb848151e08980fe4
fracomartino <francescozuliani11@gmail.com>
2024-04-25 09:54:32 +0200

    added the structure of ModelController

---
+++ app_dev/src/main/java/Server/ModelController.java
@@ -0,0 +1,16 @@
+package Server;
+import SharedWebInterfaces.Messages.ServerControllerInterface;
+
+public class ModelController implements ServerControllerInterface {
+
+
+    @Override
+    public void playCard(int id, int x, int y) {
+
+    }
+
+    @Override
+    public void drawCard() {
+
+    }
+}
