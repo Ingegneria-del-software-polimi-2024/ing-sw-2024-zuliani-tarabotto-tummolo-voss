e2a1fb603422e9dc6536dcd8c71572298ff95e8e
andrea-809 <128220234+andrea-809@users.noreply.github.com>
2024-05-15 20:28:45 +0200

    Implementation of ClientController

--- app_dev/src/main/java/Client/View/ViewAPI.java
+++ app_dev/src/main/java/Client/View/ViewAPI.java
@@ -23,331 +23,222 @@ import java.util.Objects;
  *   - methods used by the server interface to modify the view
  */
 public class ViewAPI implements ViewAPI_Interface {
-    private List<PlayableCard> hand;
-    private PlayableCard starterCard;
 
-    private List<PlayableCard> goldDeck;
-    private List<PlayableCard> resourceDeck;
-    private ObjectiveCard secretObjective;
-
-    private List<ObjectiveCard> commonObjectives;
-    private List<ObjectiveCard> chooseSecretObjectives;
-    private List<PlayableCard> openGold;
-    private List<PlayableCard> openResource;
-    private TurnState state;
-    private List<String> players;
-    private String gameId;
-    private HashMap<Artifact, Integer> availableArtifacts;
-    private HashMap<Element, Integer> availableElements;
-    //the id of THIS player
-    private String playerId;
-    private String turnPlayer;
-    private String pawnColor;
-    private List<Coordinates> availablePlaces;
-    //an array of 3 booleans indicating which of the cards in the player's hand can be placed(due to placementConstraint)
-    private boolean[] canBePlaced;
-    //the disposition of all players' are stored here
-    private HashMap< String, HashMap< Coordinates, PlayableCard> > dispositions;
-    //the points of all players are store here
-    private HashMap< String , Integer> points;
     private UI ui;
-    private ClientAPI_GO clientAPIGo;
+    private ViewModel viewModel;
 
-    public ViewAPI() {//TODO: clientAPI_GO deve essere passato come parametro
-
-        availableElements = new HashMap<>();
-        availableArtifacts = new HashMap<>();
-        //we initialize both hashmap to zero
-        for(Element el : Element.values()) {
-            availableElements.put(el, 0);
-        }
-        for(Artifact ar : Artifact.values()){
-            availableArtifacts.put(ar, 0);
-        }
-
+    public ViewAPI() {
         this.ui = new TUI(this);
-        this.commonObjectives = new ArrayList<>();
-        this.chooseSecretObjectives = new ArrayList<>();
+        this.viewModel = new ViewModel(ui);
     }
+    //TODO: clientAPI_GO deve essere passato come parametro
 
     public void setClientAPIGo(ClientAPI_GO clientAPI_GO){
-        clientAPIGo = clientAPI_GO;
-        System.out.println("helo");
+        viewModel.setClientAPIGo(clientAPI_GO);
     }
-
-    //////////////////////// INTERFACE METHODS //////////////////////////////////////////////////////////////////////////////////////
-
 
     /////////// from CLIENT to SERVER  ACTIONS ////////////////////////////////////////////////////////////////////////////////////
     //all this methods create a new MessageFromClient object containing an execute() method with the call to a specific method of ModelController
     public void playStarterCard(){
-        clientAPIGo.sendToServer( new PlayStarterCardMessage( starterCard.getFaceSide(), playerId));
+        viewModel.playStarterCard();
     }
 
-    public void chooseSecretObjective(String chosenObjective){
-        clientAPIGo.sendToServer(new ChooseSecreteObjMessage(chosenObjective, playerId));
-    }
+//    public void chooseSecretObjective(String chosenObjective){
+//        viewModel.chooseSecretObjective(chosenObjective);
+//    }
 
-    public void playCard(PlayableCard c, int x, int y) {
-        clientAPIGo.sendToServer( new PlayCardMessage(c.getId(), x, y, c.getFaceSide()));
-    }
+    public void playCard(PlayableCard c, int x, int y) {viewModel.playCard(c,x,y);}
 
-    public void drawCard(int cardSource){
-        clientAPIGo.sendToServer( new DrawCardMessage(cardSource));
-    }
+    public void drawCard(int cardSource){viewModel.drawCard(cardSource);}
 
     @Override
-    public void readyToPlay(){
-        clientAPIGo.sendToServer( new ReadyToPlayMessage());
-    }
+    public void readyToPlay(){viewModel.readyToPlay();}
 
 
-
-    /////////// from SERVER to CLIENT ACTIONS ////////////////////////////////////////////////////////////////////////////////////
+//    /////////// from SERVER to CLIENT ACTIONS ////////////////////////////////////////////////////////////////////////////////////
     @Override
     public void setState(TurnState state) {
-        this.state = state;
-        //System.out.println(state.toString());
-        this.state.display( ui);
+        viewModel.setState(state);
     }
 
     @Override
-    public void setGoldDeck(List<PlayableCard> deck){goldDeck = deck;}
+    public void setGoldDeck(List<PlayableCard> deck){viewModel.setGoldDeck(deck);}
 
     @Override
-    public void setResourceDeck(List<PlayableCard> deck){resourceDeck = deck;}
+    public void setResourceDeck(List<PlayableCard> deck){viewModel.setResourceDeck(deck);}
 
 
     @Override
-    public void setPlayers(List<String> players){
-        this.players = players;
-
-        //after we receive the array containing the unique nicknames of the players, we initialize
-        //the two hashmaps containing information about all players
-        this.dispositions = new HashMap<>();
-        this.points = new HashMap<>();
-        for(String p : players){
-            dispositions.put(p, new HashMap<>());
-            points.put(p, 0);
-        }
-    }
+    public void setPlayers(List<String> players) {viewModel.setPlayers(players);}
 
     @Override
-    public void setGameId(String gameId) { this.gameId = gameId;}
+    public void setGameId(String gameId) {viewModel.setGameId(gameId);}
 
     //the player is given his starterCard, he will then have to place it
     @Override
-    public void setStarterCard(PlayableCard starterCard){
-        //placeable is set to false because for starterCard it doesn't matter
-        System.out.println(starterCard.getId());
-        this.starterCard = starterCard;
-    }
+    public void setStarterCard(PlayableCard starterCard){viewModel.setStarterCard(starterCard);}
 
     @Override
-    public void setHand(List<PlayableCard> hand){
-        this.hand = hand;
-    }
+    public void setHand(List<PlayableCard> hand){viewModel.setHand(hand);}
 
     //the player is given the two ObjectiveCard cards from which he can choose his secretObjective
     @Override
-    public void setSecretObjectives(ObjectiveCard obj1, ObjectiveCard obj2){
-        chooseSecretObjectives.add(0, obj1);
-        chooseSecretObjectives.add(1, obj2);
-    }
+    public void setSecretObjectives(ObjectiveCard obj1, ObjectiveCard obj2){viewModel.setSecretObjectives(obj1, obj2);}
 
     //the player chooses his secretObjective
     @Override
-    public void setSecretObjective(ObjectiveCard secretObjective){
-        //this.secretObjective = secretObjective;
-        chooseSecretObjective(String.valueOf(secretObjective.getId()));
-    }
+    public void setSecretObjective(ObjectiveCard secretObjective){viewModel.setSecretObjective(secretObjective);}
 
 
     //TODO: rendere più chiara la scelta di secretObjective, confirmSecretObjective potrebbe essere eliminato
 
     @Override
     public void confirmSecretObjective(ObjectiveCard secretObjective){
-        this.secretObjective = secretObjective;
+        viewModel.confirmSecretObjective(secretObjective);
     }
 
 
     //the player's points are updated
     @Override
     public void setPoints(String player, int points) {
-        //
-        this.points.put(player, points);
+        viewModel.setPoints(player, points);
     }
 
     @Override
     public void updateArtifacts(HashMap<Artifact, Integer> artifacts) {
-        for(Artifact a : artifacts.keySet()){
-            availableArtifacts.put(a, artifacts.get(a));
-        }
+        viewModel.updateArtifacts(artifacts);
     }
 
     @Override
     public void updateElements(HashMap<Element, Integer> elements) {
-        for(Element e : elements.keySet()){
-            availableElements.put(e, elements.get(e));
-        }
+        viewModel.updateElements(elements);
     }
 
     //TODO: togliere sta cagata
     @Override
     public void updateCardSource(List<PlayableCard> deck, int cardSource) {
-
-        switch (cardSource) {
-            case 1:
-                goldDeck = deck;
-                break;
-            case 4:
-                resourceDeck = deck;
-                break;
-            case 2:
-                openGold = deck;
-                //we update openGold, this means that the first card of goldDeck has been turned and put in opendGold
-                // that is why we call goldDeck.remove(0)
-                goldDeck.remove(0);
-                break;
-            case 3:
-                openGold = deck;
-                goldDeck.remove(0);
-                break;
-            case 5:
-                openResource = deck;
-                resourceDeck.remove(0);
-                break;
-            case 6:
-                openResource = deck;
-                resourceDeck.remove(0);
-                break;
-        }
-
+        viewModel.updateCardSource(deck, cardSource);
     }
 
     @Override
     public void endGame(HashMap<String, Integer> finalPoints) {
-
+        viewModel.endGame(finalPoints);
     }
 
     @Override
-    public void setPawnColor(String pawnColor) {
-        this.pawnColor = pawnColor;
-    }
+    public void setPawnColor(String pawnColor) {viewModel.setPawnColor(pawnColor);}
 
     @Override
     public void setCommonObjectives(ObjectiveCard commonObjective1, ObjectiveCard commonObjective2){
-        commonObjectives.add(commonObjective1);
-        commonObjectives.add(commonObjective2);
+        viewModel.setCommonObjectives(commonObjective1, commonObjective2);
     }
 
     @Override
     public void setOpenGold(List<PlayableCard> openGold){
-        this.openGold = openGold;
+        viewModel.setOpenGold(openGold);
     }
 
     @Override
     public void setOpenResource(List<PlayableCard> openResource){
-        this.openResource = openResource;
+        viewModel.setOpenResource(openResource);
     }
 
     @Override
     public void setAvailablePlaces(List<Coordinates> availablePlaces){
-        this.availablePlaces = availablePlaces;
+        viewModel.setAvailablePlaces(availablePlaces);
     }
 
     @Override
     public void setCanBePlaced(boolean[] canBePlaced){
-        this.canBePlaced = canBePlaced;
+        viewModel.setCanBePlaced(canBePlaced);
     }
     @Override
     public void setDisposition(String player, HashMap<Coordinates, PlayableCard> disposition){
-        this.dispositions.put(player, disposition);
+        viewModel.setDisposition(player, disposition);
     }
 
 
     public void setPlayerId(String playerId){
-        this.playerId = playerId;
+        viewModel.setPlayerId(playerId);
     }
 
     @Override
     public boolean getMyTurn() {
-        if(turnPlayer.equals(playerId)) return true;
-        return false;
+        return viewModel.getMyTurn();
     }
 
 
-    ////////////////////////////////// GETTER METHODS //////////////////////////////////////////////////////////////////////////////
+//    ////////////////////////////////// GETTER METHODS //////////////////////////////////////////////////////////////////////////////
 
     /**
      * returns the disposition of THIS player
      * @return
      */
     public HashMap<Coordinates, PlayableCard> getDisposition(){
-        return dispositions.get(this.playerId);
+        return viewModel.getDisposition();
     }
 
     public PlayableCard getStarterCard(){
-        return this.starterCard;
+        return viewModel.getStarterCard();
     }
 
-    public List<PlayableCard> getHand(){return hand;}
+    public List<PlayableCard> getHand(){return viewModel.getHand();}
 
     public List<ObjectiveCard> getCommonObjectives() {
-        return commonObjectives;
+        return viewModel.getCommonObjectives();
     }
 
     public List<ObjectiveCard> getChooseSecretObjectives() {
-        return chooseSecretObjectives;
+        return viewModel.getChooseSecretObjectives();
     }
     public List<Coordinates> getAvailablePlaces(){
-        return availablePlaces;
+        return viewModel.getAvailablePlaces();
     }
 
     public boolean[] getCanBePlaced() {
-        return canBePlaced;
+        return viewModel.getCanBePlaced();
     }
 
     public HashMap<String, Integer> getPoints() {
-        return points;
+        return viewModel.getPoints();
     }
 
     public String getPlayerId() {
-        return playerId;
+        return viewModel.getPlayerId();
     }
 
     public HashMap<Artifact, Integer> getAvailableArtifacts() {
-        return availableArtifacts;
+        return viewModel.getAvailableArtifacts();
     }
 
     public HashMap<Element, Integer> getAvailableElements() {
-        return availableElements;
+        return viewModel.getAvailableElements();
     }
 
     public List<PlayableCard> getGoldDeck() {
-        return goldDeck;
+        return viewModel.getGoldDeck();
     }
 
     public List<PlayableCard> getResourceDeck() {
-        return resourceDeck;
+        return viewModel.getResourceDeck();
     }
 
     public List<PlayableCard> getOpenGold() {
-        return openGold;
+        return viewModel.getOpenGold();
     }
 
     public List<PlayableCard> getOpenResource() {
-        return openResource;
+        return viewModel.getOpenResource();
     }
 
     public String getTurnPlayer() {
-        return turnPlayer;
+        return viewModel.getTurnPlayer();
     }
 
     @Override
     public void setTurnPlayer(String turnPlayer) {
-        this.turnPlayer = turnPlayer;
+        viewModel.setTurnPlayer(turnPlayer);
     }
 
-    public ObjectiveCard getSecretObjective(){return secretObjective;}
+    public ObjectiveCard getSecretObjective(){return viewModel.getSecretObjective();}
 }
---
+++ app_dev/src/main/java/Client/View/ViewModel.java
@@ -0,0 +1,317 @@
+package Client.View;
+
+import Client.UI.UI;
+import Client.Web.ClientAPI_GO;
+import SharedWebInterfaces.Messages.MessagesFromClient.toModelController.*;
+import model.GameState.TurnState;
+import model.cards.ObjectiveCard;
+import model.cards.PlayableCards.PlayableCard;
+import model.enums.Artifact;
+import model.enums.Element;
+import model.placementArea.Coordinates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ViewModel {
+    private List<PlayableCard> hand;
+    private PlayableCard starterCard;
+
+    private List<PlayableCard> goldDeck;
+    private List<PlayableCard> resourceDeck;
+    private ObjectiveCard secretObjective;
+
+    private List<ObjectiveCard> commonObjectives;
+    private List<ObjectiveCard> chooseSecretObjectives;
+    private List<PlayableCard> openGold;
+    private List<PlayableCard> openResource;
+    private TurnState state;
+    private List<String> players;
+    private String gameId;
+    private HashMap<Artifact, Integer> availableArtifacts;
+    private HashMap<Element, Integer> availableElements;
+    //the id of THIS player
+    private String playerId;
+    private String turnPlayer;
+    private String pawnColor;
+    private List<Coordinates> availablePlaces;
+    //an array of 3 booleans indicating which of the cards in the player's hand can be placed(due to placementConstraint)
+    private boolean[] canBePlaced;
+    //the disposition of all players' are stored here
+    private HashMap< String, HashMap< Coordinates, PlayableCard> > dispositions;
+    //the points of all players are store here
+    private HashMap< String , Integer> points;
+
+    private ClientAPI_GO clientAPIGo;
+    private UI ui;
+
+    public ViewModel(UI ui) {
+        availableElements = new HashMap<>();
+        availableArtifacts = new HashMap<>();
+        //we initialize both hashmap to zero
+        for(Element el : Element.values()) {
+            availableElements.put(el, 0);
+        }
+        for(Artifact ar : Artifact.values()){
+            availableArtifacts.put(ar, 0);
+        }
+
+        this.commonObjectives = new ArrayList<>();
+        this.chooseSecretObjectives = new ArrayList<>();
+
+        this.ui = ui;
+    }
+    public void setClientAPIGo(ClientAPI_GO clientAPI_GO){
+        clientAPIGo = clientAPI_GO;
+        System.out.println("helo");
+    }
+    /////////// from CLIENT to SERVER  ACTIONS ////////////////////////////////////////////////////////////////////////////////////
+    //all this methods create a new MessageFromClient object containing an execute() method with the call to a specific method of ModelController
+    public void playStarterCard(){
+        clientAPIGo.sendToServer( new PlayStarterCardMessage( starterCard.getFaceSide(), playerId));
+    }
+
+    private void chooseSecretObjective(String chosenObjective){
+        clientAPIGo.sendToServer(new ChooseSecreteObjMessage(chosenObjective, playerId));
+    }
+
+    public void playCard(PlayableCard c, int x, int y) {
+        clientAPIGo.sendToServer( new PlayCardMessage(c.getId(), x, y, c.getFaceSide()));
+    }
+
+    public void drawCard(int cardSource){
+        clientAPIGo.sendToServer( new DrawCardMessage(cardSource));
+    }
+
+    public void readyToPlay(){
+        clientAPIGo.sendToServer( new ReadyToPlayMessage());
+    }
+
+
+
+    /////////// from SERVER to CLIENT ACTIONS ////////////////////////////////////////////////////////////////////////////////////
+    public void setState(TurnState state) {
+        this.state = state;
+        //System.out.println(state.toString());
+        this.state.display(ui);
+    }
+
+    public void setGoldDeck(List<PlayableCard> deck){goldDeck = deck;}
+
+
+    public void setResourceDeck(List<PlayableCard> deck){resourceDeck = deck;}
+
+
+    public void setPlayers(List<String> players){
+        this.players = players;
+
+        //after we receive the array containing the unique nicknames of the players, we initialize
+        //the two hashmaps containing information about all players
+        this.dispositions = new HashMap<>();
+        this.points = new HashMap<>();
+        for(String p : players){
+            dispositions.put(p, new HashMap<>());
+            points.put(p, 0);
+        }
+    }
+
+    public void setGameId(String gameId) { this.gameId = gameId;}
+
+    //the player is given his starterCard, he will then have to place it
+    public void setStarterCard(PlayableCard starterCard){
+        //placeable is set to false because for starterCard it doesn't matter
+        System.out.println(starterCard.getId());
+        this.starterCard = starterCard;
+    }
+
+    public void setHand(List<PlayableCard> hand){
+        this.hand = hand;
+    }
+
+    //the player is given the two ObjectiveCard cards from which he can choose his secretObjective
+    public void setSecretObjectives(ObjectiveCard obj1, ObjectiveCard obj2){
+        chooseSecretObjectives.add(0, obj1);
+        chooseSecretObjectives.add(1, obj2);
+    }
+
+    //the player chooses his secretObjective
+    public void setSecretObjective(ObjectiveCard secretObjective){
+        //this.secretObjective = secretObjective;
+        chooseSecretObjective(String.valueOf(secretObjective.getId()));
+    }
+
+
+    //TODO: rendere più chiara la scelta di secretObjective, confirmSecretObjective potrebbe essere eliminato
+
+    public void confirmSecretObjective(ObjectiveCard secretObjective){
+        this.secretObjective = secretObjective;
+    }
+
+
+    //the player's points are updated
+    public void setPoints(String player, int points) {
+        //
+        this.points.put(player, points);
+    }
+
+    public void updateArtifacts(HashMap<Artifact, Integer> artifacts) {
+        for(Artifact a : artifacts.keySet()){
+            availableArtifacts.put(a, artifacts.get(a));
+        }
+    }
+
+    public void updateElements(HashMap<Element, Integer> elements) {
+        for(Element e : elements.keySet()){
+            availableElements.put(e, elements.get(e));
+        }
+    }
+
+    //TODO: togliere sta cagata
+    public void updateCardSource(List<PlayableCard> deck, int cardSource) {
+
+        switch (cardSource) {
+            case 1:
+                goldDeck = deck;
+                break;
+            case 4:
+                resourceDeck = deck;
+                break;
+            case 2:
+                openGold = deck;
+                //we update openGold, this means that the first card of goldDeck has been turned and put in opendGold
+                // that is why we call goldDeck.remove(0)
+                goldDeck.remove(0);
+                break;
+            case 3:
+                openGold = deck;
+                goldDeck.remove(0);
+                break;
+            case 5:
+                openResource = deck;
+                resourceDeck.remove(0);
+                break;
+            case 6:
+                openResource = deck;
+                resourceDeck.remove(0);
+                break;
+        }
+
+    }
+
+    public void endGame(HashMap<String, Integer> finalPoints) {
+
+    }
+
+    public void setPawnColor(String pawnColor) {
+        this.pawnColor = pawnColor;
+    }
+
+    public void setCommonObjectives(ObjectiveCard commonObjective1, ObjectiveCard commonObjective2){
+        commonObjectives.add(commonObjective1);
+        commonObjectives.add(commonObjective2);
+    }
+
+    public void setOpenGold(List<PlayableCard> openGold){
+        this.openGold = openGold;
+    }
+
+    public void setOpenResource(List<PlayableCard> openResource){
+        this.openResource = openResource;
+    }
+
+    public void setAvailablePlaces(List<Coordinates> availablePlaces){
+        this.availablePlaces = availablePlaces;
+    }
+
+    public void setCanBePlaced(boolean[] canBePlaced){
+        this.canBePlaced = canBePlaced;
+    }
+    public void setDisposition(String player, HashMap<Coordinates, PlayableCard> disposition){
+        this.dispositions.put(player, disposition);
+    }
+
+
+    public void setPlayerId(String playerId){
+        this.playerId = playerId;
+    }
+
+    public boolean getMyTurn() {
+        if(turnPlayer.equals(playerId)) return true;
+        return false;
+    }
+
+
+    ////////////////////////////////// GETTER METHODS //////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * returns the disposition of THIS player
+     * @return
+     */
+    public HashMap<Coordinates, PlayableCard> getDisposition(){
+        return dispositions.get(this.playerId);
+    }
+
+    public PlayableCard getStarterCard(){
+        return this.starterCard;
+    }
+
+    public List<PlayableCard> getHand(){return hand;}
+
+    public List<ObjectiveCard> getCommonObjectives() {
+        return commonObjectives;
+    }
+
+    public List<ObjectiveCard> getChooseSecretObjectives() {
+        return chooseSecretObjectives;
+    }
+    public List<Coordinates> getAvailablePlaces(){
+        return availablePlaces;
+    }
+
+    public boolean[] getCanBePlaced() {
+        return canBePlaced;
+    }
+
+    public HashMap<String, Integer> getPoints() {
+        return points;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public HashMap<Artifact, Integer> getAvailableArtifacts() {
+        return availableArtifacts;
+    }
+
+    public HashMap<Element, Integer> getAvailableElements() {
+        return availableElements;
+    }
+
+    public List<PlayableCard> getGoldDeck() {
+        return goldDeck;
+    }
+
+    public List<PlayableCard> getResourceDeck() {
+        return resourceDeck;
+    }
+
+    public List<PlayableCard> getOpenGold() {
+        return openGold;
+    }
+
+    public List<PlayableCard> getOpenResource() {
+        return openResource;
+    }
+
+    public String getTurnPlayer() {
+        return turnPlayer;
+    }
+
+    public void setTurnPlayer(String turnPlayer) {
+        this.turnPlayer = turnPlayer;
+    }
+
+    public ObjectiveCard getSecretObjective(){return secretObjective;}
+}
